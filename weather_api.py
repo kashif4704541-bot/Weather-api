609167f86c63d1fbac0804dbe3abe461b147153e
Shaheer.kas <kashif4704541@cloud.neduet.edu.pk>
2025-08-01 14:45:18 +0500

    weather_api.py

---
+++ weather_api.py
@@ -0,0 +1,25 @@
+import requests
+
+Api_key = "463c76d6ee7e43f6a9f92708252907"
+city = input("Enter the city: ")
+url = f"http://api.weatherapi.com/v1/current.json?key={Api_key}&q={city}&aqi=no"
+
+response = requests.get(url)
+data = response.json()
+
+if (response.status_code==200):
+    location = data['location']['name']
+    country = data['location']['country']
+    temp_c = data['current']['temp_c']
+    condition = data['current']['condition']['text']
+    humidity = data['current']['humidity']
+    wind_kph = data['current']['wind_kph']
+
+    print(f"Weather in {location}, {country}:")
+    print(f"Temperature: {temp_c}°C")
+    print(f"Condition: {condition}")
+    print(f"Humidity: {humidity}%")
+    print(f"Wind Speed: {wind_kph} kph")
+
+else: 
+    print("Error", data.get("message","Unable to fetch weather!"))
